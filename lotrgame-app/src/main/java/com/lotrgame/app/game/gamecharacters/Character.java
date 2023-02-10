3418bef5ea3b16cf966c50fd8b02d386bfbddef1
Andres Cordova <andres.cordova@telusinternational.com>
2023-02-09 20:15:29 -0600

    Modifying classes

--- lotrgame-app/src/main/java/com/lotrgame/app/game/gamecharacters/Character.java
+++ lotrgame-app/src/main/java/com/lotrgame/app/game/gamecharacters/Character.java
@@ -1,5 +1,18 @@
 package com.lotrgame.app.game.gamecharacters;
 
 public abstract class Character {
-    
+    protected String name;
+    protected int healthPoint;
+    protected int position;
+    protected boolean aliveStatus;
+
+    public void atack(){
+        //TODO
+    }
+
+    public void getAtack(){
+        //TODO
+    }
+
+    public abstract int randomPower();
 }
